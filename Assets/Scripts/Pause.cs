4745bbec6201f30af107d2673149c73221d02c32
AIE\s166149 <Johnfaz1994@hotmail.com>
2017-11-23 14:10:19 +1030

    Added UI sprites, pause menu, and game over screen

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    /*
+        // Use this for initialization
+        void Start () {
+
+        }
+   */
+
+        // Update is called once per frame
+    void Update ()
+    {
+        foreach (InputDevice device in InputManager.Devices)
+        {
+            if (device.Action4.WasPressed)
+            {
+                PauseGame();
+            }
+            
+        }
+
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+}
