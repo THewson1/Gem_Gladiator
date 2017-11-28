c988f80127e015a9be65334d6cf4243c6d3443a3
AIE\Stephen King <thekings.stephen@gmail.com>
2017-11-28 12:17:18 +1030

    Fixed End game stuff

--- Assets/Scripts/EndGameCondition.cs
+++ Assets/Scripts/EndGameCondition.cs
@@ -13,11 +13,14 @@ public class EndGameCondition : MonoBehaviour {
     public bool m_coop;
     public bool m_versus;
     public int m_amountOfGemsRequiredToWin = 0;
+    public GameObject m_gameOverScreen;
     [HideInInspector]
     public bool m_gameOver = false;
 
     private int m_gameMode;
     private GameObject[] m_players;
+    [HideInInspector]
+    public int m_finalScore = 0;
 	
     void Start ()
     {
@@ -31,75 +34,87 @@ public class EndGameCondition : MonoBehaviour {
     }
 
     void Update () {
-		switch(m_gameMode)
-        {
-            //single player
-            case (1):
-                foreach (GameObject player in m_players)
-                {
-                    if (player.GetComponent<PlayerDeathLogic>().m_lives <= 0)
-                        m_gameOver = true;
-                }
-            break;
-            //co-op
-            case (2):
-                m_gameOver = true;
-                foreach (GameObject player in m_players)
-                {
-                    PlayerDeathLogic playerDeathLogic = player.GetComponent<PlayerDeathLogic>();
-                    if (playerDeathLogic.m_lives > 0)
-                    {
-                        m_gameOver = false;
-                        break;
-                    }
-                }
-                break;
-            //verses
-            case (3):
-                foreach (GameObject player in m_players)
-                {
-                    PlayerDeathLogic playerLogic;
-                    if (playerLogic = player.GetComponent<PlayerDeathLogic>())
-                    {
-                        int playerNumber = player.GetComponent<PlayerInput>().m_playerNumber;
-                        if (playerLogic.m_lives <= 0)
-                            m_gameOver = true;
-                        if (GetComponent<GameController>().m_amountOfGems[playerNumber] > m_amountOfGemsRequiredToWin)
-                            m_gameOver = true;
-                    }
-                }
-                break;
-            default:
-                //default
-            break;
-        }
-
-        if (m_gameOver)
-        {
-            //show game over screen (display game over GUI)
-            Debug.Log("GameOver");
-            SceneManager.LoadScene(0); // this is temp
-            GameController gC = GetComponent<GameController>();
-            switch (m_gameMode)
+        if (!m_gameOver)
+        { 
+		    switch(m_gameMode)
             {
-                case (0):
-                    gC.CalculateFinalScore(m_players);
-                    break;
-
+                //single player
                 case (1):
-                    gC.CalculateFinalScore(m_players);
-                    break;
-
-                case (2):
                     foreach (GameObject player in m_players)
                     {
-                        if (player.GetComponent<PlayerDeathLogic>().m_lives > 0)
+                        if (player.GetComponent<PlayerDeathLogic>().m_lives <= 0)
+                            m_gameOver = true;
+                    }
+                break;
+                //co-op
+                case (2):
+                    m_gameOver = true;
+                    foreach (GameObject player in m_players)
+                    {
+                        PlayerDeathLogic playerDeathLogic = player.GetComponent<PlayerDeathLogic>();
+                        if (playerDeathLogic.m_lives > 0)
                         {
-                            // whoever is not dead has won
+                            m_gameOver = false;
+                            break;
                         }
                     }
                     break;
+                //verses
+                case (3):
+                    foreach (GameObject player in m_players)
+                    {
+                        PlayerDeathLogic playerLogic;
+                        if (playerLogic = player.GetComponent<PlayerDeathLogic>())
+                        {
+                            int playerNumber = player.GetComponent<PlayerInput>().m_playerNumber;
+                            if (playerLogic.m_lives <= 0)
+                                m_gameOver = true;
+                            if (m_amountOfGemsRequiredToWin != 0 && GetComponent<GameController>().m_amountOfGems[playerNumber] > m_amountOfGemsRequiredToWin)
+                                m_gameOver = true;
+                        }
+                    }
+                    break;
+                default:
+                    //default
+                break;
+            }
+
+            if (m_gameOver)
+            {
+                //show game over screen (display game over GUI)
+                Debug.Log("GameOver");
+                Invoke("GameOver", 5);
             }
         }
 	}
+
+    void GameOver() {
+        GameController gC = GetComponent<GameController>();
+        switch (m_gameMode)
+        {
+            case (0):
+                finalScore = gC.CalculateFinalScore(m_players);
+                m_gameOverScreen.SetActive(true);
+                Time.timeScale = 0;
+                break;
+
+            case (1):
+                finalScore = gC.CalculateFinalScore(m_players);
+                m_gameOverScreen.SetActive(true);
+                Time.timeScale = 0;
+                break;
+
+            case (2):
+                foreach (GameObject player in m_players)
+                {
+                    if (player.GetComponent<PlayerDeathLogic>().m_lives > 0)
+                    {
+                        // whoever is not dead has won
+                        m_gameOverScreen.SetActive(true);
+                        Time.timeScale = 0;
+                    }
+                }
+                break;
+        }
+    }
 }
