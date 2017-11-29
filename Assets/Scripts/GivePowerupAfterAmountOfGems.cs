07938a8bd2af272ffee2bc532e1403e24b7a844d
AIE\Stephen King <thekings.stephen@gmail.com>
2017-11-29 13:05:27 +1030

    Sort of fixed Get power up after gems (still broken)

--- Assets/Scripts/GivePowerupAfterAmountOfGems.cs
+++ Assets/Scripts/GivePowerupAfterAmountOfGems.cs
@@ -6,27 +6,42 @@ public class GivePowerupAfterAmountOfGems : MonoBehaviour {
 
     public GameObject[] m_powerups;
     public int m_requiredAmountOfGems;
-	
-	// Update is called once per frame
-	void Update () {
-        GameController gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+
+    private GameObject m_gameController;
+
+    private void Start()
+    {
+        m_gameController = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        GameController gc = m_gameController.GetComponent<GameController>();
+
         int playerToGetPowerup = -1;
-        foreach(KeyValuePair<int, int> playernumber in gameController.m_amountOfGems)
+        int pn = -1;
+        for (int i = 0; i < gc.m_listOfPlayers.Count; i++)
         {
-            if (playernumber.Value >= m_requiredAmountOfGems)
+            pn = gc.m_listOfPlayers[i].GetComponent<PlayerInput>().m_playerNumber;
+            Debug.Log(gc.m_amountOfGems[pn] + " : PLayer" + pn);
+            if (gc.m_amountOfGems[pn] >= m_requiredAmountOfGems)
             {
-                playerToGetPowerup = playernumber.Key;
-                gameController.m_amountOfGems[playernumber.Key] = 0;
+                playerToGetPowerup = pn;
+                gc.m_amountOfGems[pn] = 0;
+            }
+            else
+            {
+                pn = -1;
             }
         }
         if (playerToGetPowerup != -1)
         {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            for (int i = 0; i < players.Length; i++)
+            for (int i = 0; i < gc.m_listOfPlayers.Count; i ++)
             {
-                if (players[i].GetComponent<PlayerInput>().m_playerNumber == playerToGetPowerup)
+                if (gc.m_listOfPlayers[i].GetComponent<PlayerInput>().m_playerNumber == pn)
                 {
-                    AddRandomPowerup(players[i]);
+                    AddRandomPowerup(gc.m_listOfPlayers[i]);
+                    Debug.Log(gc.m_listOfPlayers[i]);
                 }
             }
         }
